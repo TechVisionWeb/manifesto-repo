afe873bacb6993041f7266b2457db6955bc78e94
techvisionweb <techvisionweb@yahoo.com>
2026-03-16 22:40:35 +0000

    add another counter in hero to build momentum

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
            fetch("https://petition.parliament.uk/petitions/747234.json")
            .then(response => response.json() )
-           .then(obj => {document.getElementById("signature-count").innerHTML
+           .then(obj => {document.getElementById("signature-count").innerHTML = document.getElementById("signature-count-hero").innerHTML
              = obj.data.attributes.signature_count;  })
              .catch(error => console.log(error));
 
